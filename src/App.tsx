10559db49a97600fe8c3a0a6837bd651fe9f7303
Gabriel Carriel <ga123carriel2015@gmail.com>
2022-11-03 15:32:02 -0300

    feat: página de erro 404 NotFound criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { DashboardPage } from "./pages/DashboardPage";
+import { LandingPage } from "./pages/LadingPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 import { Register } from "./pages/RegisterPage";
 
 function App() {
   return (
     <>
-      <Register />
+      <NotFoundPage />
     </>
   );
 }
