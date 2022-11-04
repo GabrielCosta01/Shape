6f0cd4203648dd140af133bbfa53eb72b95a4a71
luis <luisgabriel.modena326@gmail.com>
2022-11-04 09:37:12 -0300

    feat: create toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { DashboardPage } from "./pages/DashboardPage";
-import { Register } from "./pages/RegisterPage";
+import { ToastContainer } from "./components/ToastContainer/ToastContainer";
 
 function App() {
   return (
     <>
-      <Register />
+      <ToastContainer />
     </>
   );
 }
---
+++ src/stores/toastStore.ts
@@ -0,0 +1,33 @@
+import create from "zustand";
+import { v4 as uuid } from "uuid";
+
+export interface IToast {
+  id: string;
+  message: string;
+  mode: modeColor;
+  ms: number;
+}
+
+type modeColor = "success" | "info" | "warning" | "error";
+
+interface IToastStore {
+  listToast: IToast[];
+  toast: (message: string, mode: modeColor, ms: number) => void;
+  removeToast: (id: string) => void;
+}
+
+export const toastStore = create<IToastStore>((set) => ({
+  listToast: [],
+
+  toast: (message: string, mode: modeColor, ms: number) => {
+    const newToast = { id: uuid(), message, mode, ms };
+
+    set((state) => ({ listToast: [...state.listToast, newToast] }));
+  },
+
+  removeToast: (id) => {
+    set((state) => ({
+      listToast: state.listToast.filter((toast) => toast.id !== id),
+    }));
+  },
+}));
