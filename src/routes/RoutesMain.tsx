56f1927ecf3bdd02b72b6f4021cad66e00f5e410
Breno Leite <brenoleitepereira@gmail.com>
2022-11-04 08:28:35 -0300

    Feat: LoginPage complete

--- src/routes/RoutesMain.tsx
+++ src/routes/RoutesMain.tsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import { RegisterPage } from "../pages/RegisterPage";
 import LoginPage from "../pages/LoginPage";
+import { DashboardPage } from "../pages/DashboardPage";
 
 const RoutesMain = () => {
   return (
-    <Routes>
-      <Route path="/cadastro" element={<RegisterPage />} />
-      <Route path="/login" element={<LoginPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/cadastro" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </>
   );
 };
 
--- src/stores/loginUserStore.tsx
+++ src/stores/loginUserStore.tsx
@@ -1,22 +1,32 @@
+import { Navigate, useNavigate } from "react-router-dom";
 import create from "zustand";
 
-import { IData } from "../components/Login/FormLogin";
+import { IData } from "../components/Login/formLogin";
 import { api } from "../services/api";
 
 interface ILoginStore {
   isLoading: boolean;
   loginUser: (data: IData) => void;
+  isLogged: boolean;
 }
 
 export const loginUserStore = create<ILoginStore>((set) => ({
+  isLogged: false,
   isLoading: false,
   loginUser: async (data: IData) => {
     try {
       set(() => ({ isLoading: true }));
+
       const request = await api.post("login", data);
+
       console.log(request);
+
       set(() => ({ isLoading: false }));
+
       localStorage.setItem("@shape:token", request.data.accessToken);
+      localStorage.setItem("@shape:userId", request.data.user.id);
+
+      set(() => ({ isLogged: true }));
     } catch (error) {
       console.error(error);
       set(() => ({ isLoading: false }));
